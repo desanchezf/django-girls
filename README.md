ae2634eed8ecdde6afe173eb402c870d00ffe6e4
desanchezf <davidsfernandez7@outlook.es>
2022-05-20 13:54:30 +0200

    Finished adnin site and published some sample posts

--- README.md
+++ README.md
@@ -5,10 +5,32 @@ A sample django app
 ### Commands 
 
 ```Python
+  # Make migrations, add model changes to the project
   python manage.py migrate
 ```
 
+*Run makemigration blog and migrate blog together*
+
+```Python
+  # Make migrations, for models
+  python manage.py makemigrations [blog]
+```
+
 ```Python 
+  python manage.py migrate blog
+```
+
+```Python 
+  # Run the server localhost:8000
   python manage.py runserver [i.e. 0.0.0.0:8000 localhost]
 ```
 
+```Python
+  # App init 
+  python manage.py startapp blog
+```
+
+```Python 
+  # Create superuser for administration
+  python3 manage.py createsuperuser
+```
---
+++ blog/models.py
@@ -0,0 +1,38 @@
+
+from django.conf import settings
+from django.db import models
+from django.forms import DateTimeField
+from django.utils import timezone
+
+
+# Create your models here.
+
+class Post(models.Model): 
+
+    # Attributes
+    author: str = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title: str = models.CharField(max_length=200)
+    text: str = models.TextField()
+    created_date = models.DateTimeField(
+        default = timezone.now
+    )
+    published_date = models.DateTimeField(
+        blank=True, 
+        null=True
+    )
+
+    # Methods
+    def publish(self) -> None: 
+
+        self.published_date = timezone.now()
+        self.save()
+    
+    def __str__(self) -> str:
+
+        object = "author" + str(self.author) + "\n"
+        object += "title" + str(self.title) + "\n"
+        object += "text" + str(self.text) + "\n"
+        object += "created_date" + str(self.created_date) + "\n"
+        object += "created_date" + str(self.published_date) + "\n"
+        
+        return object
